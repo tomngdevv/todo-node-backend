f67309c79275812350fa958761c7a65132b87663
ToMMnG <tomng2794@gmail.com>
2022-11-29 00:12:17 +0800

    wip

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const routes = require('./routes/index')
+
+require('dotenv').config
+
 
 const app = express();
-const port = 3000;
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,14 +20,52 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users.routes');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`)
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.MONGO_URI}`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log(`connection error: ${err.stack}`));
+db.once('open', () => {
+  console.log('We are connected to the database');
+
+  app.use('/api', routes);
+
+  app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+  });
+
+  // error handler middleware
+  app.use((error, req, res, next) => {
+    logger.error(`Error occured: ${error}`);
+    res.status(error.status || 500).send({
+      error: {
+        status: error.status || 500,
+        message: error.message || 'Internal Server Error',
+      },
+    });
+  });
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`)
 })
+
+process.on('SIGINT', () => {
+  console.log('SIGINT RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const user_routes = require('./users.routes')
+
+
+router.use('/user', user_routes);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
